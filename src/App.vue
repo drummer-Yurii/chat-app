088f97fdb82a4caa311355db9818302ef6f7471e
Yurii Rybachok <drummer2009@ukr.net>
2023-10-30 19:54:02 +0200

    add logic for login

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,43 @@
 <template>
-  <div class="view login">
-    <form class="login-form">
+  <div class="view login" v-if="state.username === '' || state.username === null">
+    <form class="login-form" @submit.prevent="Login">
       <div class="form-inner">
         <h1>Login To Chat</h1>
         <label for="username">Username</label>
-        <input type="text" placeholder="Please enter your username..." />
+        <input type="text" v-model="inputUsername" placeholder="Please enter your username..." />
         <input type="submit" value="Login" />
       </div>
     </form>
   </div>
-  <div class="view chat"></div>
+  <div class="view chat" v-else>
+    <h1>Chat View</h1>
+  </div>
 </template>
 
 <script>
+import { reactive, onMounted, ref } from 'vue';
 import db from './db';
 export default {
   name: 'App',
   setup() {
-    return {};
+    const inputUsername = ref('');
+
+    const state = reactive({
+      username: '',
+      messages: [],
+    });
+
+    const Login = () => {
+      if (inputUsername.value != '' || inputUsername.value != null) {
+        state.username = inputUsername.value;
+        inputUsername.value = '';
+      }
+    }
+    return {
+      inputUsername,
+      Login,
+      state,
+    };
   },
 };
 </script>
